57b726f736043a2baefc164e41f133da6ce9545b
Priyan Ariyasinghe <pmariyasinghe@gmail.com>
2019-01-28 00:06:15 +0000

    Changes on 27/01/2019

--- Ansible/LAB/scripts/lab_show_lldp_neighbours.py
+++ Ansible/LAB/scripts/lab_show_lldp_neighbours.py
@@ -32,7 +32,6 @@ def main():
         print "Executing Show LLDP command on: %s " % switches
         response = switch.runCmds( 1, [ 'show lldp neighbors'] )
 	lldpneighbors = response[ 0 ][ 'lldpNeighbors' ]
-        print lldpneighbors
         ifile = open(sys.argv[2],'r')
 #        ifile = open("/vagrant/NetAutomation/Ansible/LAB/ConfigRepos/topology/" + switches + "_topology", 'r')   
         jneighbors = json.loads(ifile.read().decode("UTF-8").replace("u'","'").replace("'","\"").replace(" ",""))["Neighbors"]
@@ -52,7 +51,7 @@ def main():
                       print "Neighbor %s is connected to %s ---> Topology Validation %s" % ( [litem][0]['neighborDevice'], [litem][0]['port'], '= FAIL' )
                       jitem["validation"] = "FAIL"
         ifile.close()
-#        ofile = open("/vagrant/NetAutomation/Ansible/LAB/ConfigRepos/topology/" + switches + "_topology", 'w')a
+        ofile = open("/vagrant/NetAutomation/Ansible/LAB/ConfigRepos/topology/" + switches + "_topology", 'w')
         ofile = open(sys.argv[2],'w')
         json.dump(jneighbors,ofile)
         ofile.close()
