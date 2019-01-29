d62a89f19f47077c8c3a80d0892d99f5a7836001
Priyan Ariyasinghe <pmariyasinghe@gmail.com>
2019-01-29 22:53:41 +0000

    Changes on 29/01/2019 at 22:53

---
+++ Ansible/LAB/scripts/scpcopy_get-config.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+from SCPConnection import SCPConnection
+ 
+port = 22
+
+dfile = sys.argv[2].upper() + ".conf"
+base_dir = "/home/ADSERV/ansible/ansible/CFG/HPN/REPOS/LAB/"
+src = "/mnt/flash/startup-config"
+dpath = base_dir + "CURRENT/"
+dst = dpath + dfile
+
+username = sys.argv[1]
+hostname = sys.argv[2]
+password = sys.argv[3]
+
+try:
+   with open(dst, 'r') as f:
+        f.close()
+except IOError:
+   with open(dst, 'w') as f:
+        f.close()
+
+sys.stdout = open(base_dir + "LOG/" + hostname + ".log",'a')
+print "############################ STARTUP CONFIG DOWNLOAD #############################"
+print "Date and Time : " + time.strftime("%x") + " " + time.strftime("%X")
+print "Downloaded by : " +  username
+print "      File downloading        "
+print "\n"
+print src + '>> >>> >>' + dst
+
+sshcon = SCPConnection(hostname, username, password)
+sshcon.get(src, dst)
+sshcon.close()
